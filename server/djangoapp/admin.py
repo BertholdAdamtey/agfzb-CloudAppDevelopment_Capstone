cd4ea0244794e002ce73a6e2f25f687ac5b85e97
BertholdAdamtey <>
2023-06-03 13:04:07 -0400

    adding temporary changes to github 2

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
 
+# @admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
-
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 # Register models here
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -11,6 +11,16 @@ from django.utils.timezone import now
 # - __str__ method to print a car make object
 
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add any other fields you want for the CarMake model
+
+    def __str__(self):
+        return self.name
+
+
+
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
@@ -21,7 +31,43 @@ from django.utils.timezone import now
 # - __str__ method to print a car make object
 
 
+class CarModel(models.Model):
+    car_make = models.ForeignKey('CarMake', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    dealer_id = models.CharField(max_length=100)
+    TYPE_CHOICES = (
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices as needed
+    )
+    car_type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    year = models.DateField()
+    # Add any other fields you want for the CarModel model
+
+    def __str__(self):
+        return self.name
+
+
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+    def __init__(self, dealer_id, name, location):
+        self.dealer_id = dealer_id
+        self.name = name
+        self.location = location
+        # Add any other dealer data fields as needed
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, reviewer_name, rating, comments):
+        self.reviewer_name = reviewer_name
+        self.rating = rating
+        self.comments = comments
+        # Add any other review data fields as needed
+
+    def __str__(self):
+        return f"Review by {self.reviewer_name}, Rating: {self.rating}"
