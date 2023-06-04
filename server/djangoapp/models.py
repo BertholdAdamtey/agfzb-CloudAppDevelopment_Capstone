64473b55251b51f1df8366789f711b1e55d11fd6
BertholdAdamtey <bertholdadamteykwaku@gmail.com>
2023-06-04 12:55:32 -0400

    adding temporary files

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -50,24 +50,63 @@ class CarModel(models.Model):
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+# class CarDealer:
+#     def __init__(self, dealer_id, name, location):
+#         self.dealer_id = dealer_id
+#         self.name = name
+#         self.location = location
+#         # Add any other dealer data fields as needed
+
+#     def __str__(self):
+#         return self.name
 class CarDealer:
-    def __init__(self, dealer_id, name, location):
-        self.dealer_id = dealer_id
-        self.name = name
-        self.location = location
-        # Add any other dealer data fields as needed
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
 
     def __str__(self):
-        return self.name
+        return "Dealer name: " + self.full_name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
-class DealerReview:
-    def __init__(self, reviewer_name, rating, comments):
-        self.reviewer_name = reviewer_name
-        self.rating = rating
-        self.comments = comments
-        # Add any other review data fields as needed
+# old dealer review class
+# class DealerReview:
+#     def __init__(self, reviewer_name, rating, comments):
+#         self.reviewer_name = reviewer_name
+#         self.rating = rating
+#         self.comments = comments
+#         # Add any other review data fields as needed
 
+#     def __str__(self):
+#         return f"Review by {self.reviewer_name}, Rating: {self.rating}"
+class DealerReview:
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment
+        self.id = id
     def __str__(self):
-        return f"Review by {self.reviewer_name}, Rating: {self.rating}"
+        return f"DealerReview: ID={self.id}, Name={self.name}, Dealership={self.dealership}, Car={self.car_year} {self.car_make} {self.car_model}, Purchase Date={self.purchase_date}, Sentiment={self.sentiment}"
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from .models import CarDealer, DealerReview
 # import related models here
 from requests.auth import HTTPBasicAuth
 
@@ -9,6 +10,21 @@ from requests.auth import HTTPBasicAuth
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 
 
+def get_request(url, **kwargs):
+    print(kwargs)
+    print("GET from {} ".format(url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
+
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 
@@ -17,6 +33,25 @@ from requests.auth import HTTPBasicAuth
 # def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["rows"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer["doc"]
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+
+    return results
 
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
@@ -24,6 +59,29 @@ from requests.auth import HTTPBasicAuth
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
 
+from models import DealerReview
+
+def get_dealer_reviews_from_cf(dealer_id):
+    url = f"https://your-api-url.com/reviews?dealerId={dealer_id}"
+    response = get_request(url, dealerId=dealer_id)
+    
+    reviews = []
+    for review_data in response.json():
+        dealership = review_data.get("dealership")
+        name = review_data.get("name")
+        purchase = review_data.get("purchase")
+        review = review_data.get("review")
+        purchase_date = review_data.get("purchase_date")
+        car_make = review_data.get("car_make")
+        car_model = review_data.get("car_model")
+        car_year = review_data.get("car_year")
+        sentiment = review_data.get("sentiment")
+        review_id = review_data.get("id")
+        
+        review = DealerReview(dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, review_id)
+        reviews.append(review)
+    
+    return reviews
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
 # def analyze_review_sentiments(text):
